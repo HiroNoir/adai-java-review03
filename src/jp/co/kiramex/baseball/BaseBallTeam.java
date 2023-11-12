6c607e4d881a654cd0d9f268ecf5d77fc2e99978
Hiroaki.Yamamoto <noir.noir@hotmail.co.jp>
2023-11-12 17:53:56 +0900

    課題の仕様に「カプセル化を使うようにしましょう」と記載があるのを見過ごしておりました。申し訳ございません。改めて修正してみましたので、ご確認のほど宜しくお願いもします。 ・BaseBallTeamクラスの各フィールドの宣言にて、publucｊからprivateに修正 ・BaseBallTeamクラスに、セッターとゲッターのメソッドを追加 ・Review03クラスにて、セッターメソッドに対応した各インスタンスの代入に修正

--- src/jp/co/kiramex/baseball/BaseBallTeam.java
+++ src/jp/co/kiramex/baseball/BaseBallTeam.java
@@ -3,12 +3,44 @@ package jp.co.kiramex.baseball;
 public class BaseBallTeam {
 
     //　フィールド
-    public String name; //　チーム名
-    public int win; // 勝利数
-    public int lose; //敗北数
-    public int draw; // 引分数
+    private String name; //　チーム名
+    private int win; // 勝利数
+    private int lose; //敗北数
+    private int draw; // 引分数
 
-     //　勝率を計算するメソッド(自クラス内のフィールドを直接参照する方法)
+    //　デフォルトコンストラクタ
+    public BaseBallTeam() {
+    }
+
+    // setterメソッドを用いて値を代入
+    public void setTeamName(String name) {
+        this.name = name;
+    }
+    public void setWinNum(int win) {
+        this.win = win;
+    }
+    public void setLoseNum(int lose) {
+        this.lose = lose;
+    }
+    public void setDrawNum(int draw) {
+        this.draw = draw;
+    }
+
+    // getterメソッドを用いて値を参照
+    public String getTeamName() {
+        return name;
+    }
+    public int getWinNum() {
+        return win;
+    }
+    public int getLoseNum() {
+        return lose;
+    }
+    public int getDrawNum() {
+        return draw;
+    }
+
+    //　勝率を計算するメソッド
     public double getRate() {
         double result = (double) win / (win + lose); // int型の計算をdouble型にキャスト
         return result; // 戻り値
--- src/jp/co/kiramex/baseball/Review03.java
+++ src/jp/co/kiramex/baseball/Review03.java
@@ -7,40 +7,40 @@ public class Review03 {
         // 6チームのインスタンス化
 
         BaseBallTeam team01 = new BaseBallTeam();
-        team01.name = "東京ヤクルトスワローズ" ;
-        team01.win = 80;
-        team01.lose = 59;
-        team01.draw = 4;
+        team01.setTeamName("東京ヤクルトスワローズ") ;
+        team01.setWinNum(80);
+        team01.setLoseNum(59);
+        team01.setDrawNum(4);
 
         BaseBallTeam team02 = new BaseBallTeam();
-        team02.name = "横浜DeNAベイスターズ" ;
-        team02.win = 73;
-        team02.lose = 68;
-        team02.draw = 2;
+        team02.setTeamName("横浜DeNAベイスターズ") ;
+        team02.setWinNum(73);
+        team02.setLoseNum(68);
+        team02.setDrawNum(2);
 
         BaseBallTeam team03 = new BaseBallTeam();
-        team03.name = "阪神タイガース" ;
-        team03.win = 68;
-        team03.lose = 71;
-        team03.draw = 4;
+        team03.setTeamName("阪神タイガース") ;
+        team03.setWinNum(68);
+        team03.setLoseNum(71);
+        team03.setDrawNum(4);
 
         BaseBallTeam team04 = new BaseBallTeam();
-        team04.name = "読売ジャイアンツ" ;
-        team04.win = 68;
-        team04.lose = 72;
-        team04.draw = 3;
+        team04.setTeamName("読売ジャイアンツ") ;
+        team04.setWinNum(68);
+        team04.setLoseNum(72);
+        team04.setDrawNum(3);
 
         BaseBallTeam team05 = new BaseBallTeam();
-        team05.name = "広島東洋カープ" ;
-        team05.win = 66;
-        team05.lose = 74;
-        team05.draw = 3;
+        team05.setTeamName("広島東洋カープ") ;
+        team05.setWinNum(66);
+        team05.setLoseNum(74);
+        team05.setDrawNum(3);
 
         BaseBallTeam team06 = new BaseBallTeam();
-        team06.name = "中日ドラゴンズ" ;
-        team06.win = 66;
-        team06.lose = 75;
-        team06.draw = 2;
+        team06.setTeamName("中日ドラゴンズ") ;
+        team06.setWinNum(66);
+        team06.setLoseNum(75);
+        team06.setDrawNum(2);
 
         // 勝敗情報の表示
         team01.report();
